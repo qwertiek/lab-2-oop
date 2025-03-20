7c9731f349227348080b2b9f9ea2b3b52f0be205
qwertiek <119134870+qwertiek@users.noreply.github.com>
2025-03-21 00:44:09 +0300

    add solution to task 2

---
+++ task 2/library.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+#include <unordered_set>
+#include <string>
+
+
+#include <string>
+
+namespace template_library {
+    int longest_duplicate_subarray(const std::vector<int>& nums);
+}
